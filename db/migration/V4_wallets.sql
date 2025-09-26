ee16cb57c5a32a710861a9a24b186752f04ccd6f
Chitipat Pangma <92365933+Chi-to-se@users.noreply.github.com>
2025-09-27 00:51:43 +0700

    Add wallet_type in wallets
    Add:
    - Add wallet_type in wallets table
    
    Bug:
    1. Unique constraint FK Error to categories, payment_methods, wallets
    
    Fixed:
    - Fixed Bug 1
    
    TODO:
    - Testing FK

--- db/migration/V4_wallets.sql
+++ db/migration/V4_wallets.sql
@@ -3,6 +3,7 @@ create table if not exists public.wallets(
 	user_id int not null references public.users(user_id) on delete cascade,
 	wallet_name varchar(255) not null,
 	starting_balance numeric(12,2) not null default 0,
+	wallet_type varchar(255) not null,
 	created_at timestamptz default now() not null,
 
 	-- Prevent same wallet name in the same user
@@ -10,12 +11,12 @@ create table if not exists public.wallets(
 	constraint unique_wallets_user_pair unique(user_id, wallet_id)
 );
 
-insert into wallets(user_id, wallet_name) values
+insert into wallets(user_id, wallet_name, wallet_type) values
 (
-	1,'Wallet_1'
+	1,'Wallet_1','Cash'
 ),
 (
-    2,'first_wallet'
+    2,'first_wallet','Savings'
 );
 
 select * from wallets;
